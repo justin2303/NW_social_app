2c9e93e2d43b5e180cd6f09be5ee2574b498eeab
FleDeSou <FleanceDeSousa@gmail.com>
2024-10-07 21:49:07 -0400

    I made the changes u requested

--- lib/db_funcs/sftp_automation_script.go
+++ lib/db_funcs/sftp_automation_script.go
@@ -1,5 +1,4 @@
-
-package main
+package  db_funcs
 
 import (
 	"fmt"
@@ -11,89 +10,78 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
-func SFTP_Automation_Script() {
-	// SFTP server details use this is where u can ur details wen trying to conect to the sftp site
-	sftpServer := "199.127.62.40:8822"                               // srvr
-	sftpUser := "FreddyFazbearXXX"                                   // user
-	sftpPass := "asssniper"                                          // password
-	remoteFile := "/199.127.62.40_7240/Logs/server_log_10_05_24.txt" // remote file path - dw u dont gotta change the pathway it shud still pull current date
+func    SftpConnect()  {
+	// SFTP server details
+	sftpServer := "199.127.62.40:8822" // srvr
+	sftpUser := "FreddyFazbearXXX"     // user
+	sftpPass := "asssniper"            // pass
 
-	// this is supposed to pull the current date
+	// Get Current Date
 	currentDate := time.Now()
-	fmt.Println("Using current date:", currentDate.Format("01-02-06"))
+	dateString := currentDate.Format("01_02_06") // Format as MM-DD-YY
+	remoteFile := fmt.Sprintf("/199.127.62.40_7240/Logs/server_log_%s.txt", dateString)
 
-/*
-	*******NOT USED, INCASE YOU WOULD LIKE TO CHANGE AND USE INPUT DATE INSTEAD***********
-	// Ask for date input
-	//var inputDate string
-	//fmt.Print("Enter the date (MM-DD-YY): ")
-	//fmt.Scan(&inputDate)
+	fmt.Println("Using current date:", dateString)
+	fmt.Println("Remote file path:", remoteFile)
 
-	// Parse the date
-	//date, err := time.Parse("01-02-06", inputDate)
-	//if err != nil {
-	//	fmt.Println("Error parsing date:", err)
-	//	return
-	//}
-	******************************************************************************************
-*/
-
-
-	// format the local file name based on the current date 
-	localFile := fmt.Sprintf("server_log_%02d_%02d_%02d.txt", currentDate.Month(), currentDate.Day(), currentDate.Year()%100)
+	// Change local file object with current date
+	localFile := fmt.Sprintf("./logs/server_log_%02d_%02d_%02d.txt", currentDate.Month(), currentDate.Day(), currentDate.Year()%100)
 	fmt.Println("Local file will be saved as:", localFile)
 
-/// connection phase ///
+	// Create logs directory if it doesn't exist
+	err := os.MkdirAll("logs", os.ModePerm)
+	if err != nil {
+		fmt.Println("Failed to create logs directory:", err)
+		return
+	}
 
-	// create SSH config
+	// Connection phase
+	// Create SSH config
 	config := &ssh.ClientConfig{
 		User: sftpUser,
 		Auth: []ssh.AuthMethod{
 			ssh.Password(sftpPass),
 		},
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // this is probably super unsafe but i cudnt figure out how to do shit with the hsot key lol
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // WARNING: This is probably unsafe lol
 	}
 
-	// connect to the SFTP site 
+	// Connect to the SFTP site
 	conn, err := ssh.Dial("tcp", sftpServer, config)
 	if err != nil {
-		fmt.Println("failure when connecting to sftp site: ", err)
+		fmt.Println("Failure when connecting to SFTP site:", err)
 		return
 	}
 	defer conn.Close()
 
-
-
-	// create a new SFTP client
+	// Create a new SFTP client
 	sftpClient, err := sftp.NewClient(conn)
 	if err != nil {
-		fmt.Println("failed wen trying to create sftp site: ", err)
+		fmt.Println("Failed when trying to create SFTP client:", err)
 		return
 	}
 	defer sftpClient.Close()
 
-	// open the remote file
+	// Open the remote file
 	remoteFileHandle, err := sftpClient.Open(remoteFile)
 	if err != nil {
-		fmt.Println("failed when trying to connect to the pathway: ", err)
+		fmt.Println("Failed when trying to open the remote file:", err)
 		return
 	}
 	defer remoteFileHandle.Close()
-	fmt.Println("remote file SUCCESS!!!: ")
+	fmt.Println("Remote file SUCCESS!!!")
 
-///copy over file phase //// 
+	// Copy over file phase
 
-
-	// create local file
+	// Local file creation
 	outFile, err := os.Create(localFile)
 	if err != nil {
-		fmt.Println("failure when trying to create local file:", err)
+		fmt.Println("Failure whem trying to create local file: ", err)
 		return
 	}
 	defer outFile.Close()
 	fmt.Println("Local file successfully created")
 
-	// copy the contents of the remote file to the local file
+	// Copy the contents of the remote file to the local file
 	_, err = io.Copy(outFile, remoteFileHandle)
 	if err != nil {
 		fmt.Println("Failure when trying to copy the contents of the file:", err)
@@ -102,3 +90,4 @@ func SFTP_Automation_Script() {
 
 	fmt.Println("TOTAL SUCCESS!!!!! 61E WINS AGAIN!!!:", localFile)
 }
+
